91b24718c58e92742f131c319e237aa507cbba9d
Muskan Sharma <muskansharma.ms66@gmail.com>
2026-09-19 14:36:35 +0530

    Delete src/main/java/com/logtriage/engine directory

--- src/main/java/com/logtriage/engine/src/main/java/com/logtriage/engine/config/src/main/java/com/logtriage/engine/config/src/main/java/com/logtriage/engine/config/src/main/java/com/logtriage/engine/config/src/main/java/com/logtriage/engine/config/src/main/java/com/logtriage/engine/consumer/src/main/java/com/logtriage/engine/controller/src/main/java/com/logtriage/engine/entity/src/main/java/com/logtriage/engine/exception/src/main/java/com/logtriage/engine/exception/src/main/java/com/logtriage/engine/exception/src/main/java/com/logtriage/engine/model/src/main/java/com/logtriage/engine/model/src/main/java/com/logtriage/engine/model/LogLevel.java
+++
@@ -1,9 +0,0 @@
-package com.logtriage.engine.model;
-
-public enum LogLevel {
-    TRACE, DEBUG, INFO, WARN, ERROR, FATAL;
-
-    public boolean requiresTriage() {
-        return this == ERROR || this == FATAL;
-    }
-}
--- src/main/java/com/logtriage/engine/src/main/java/com/logtriage/engine/config/src/main/java/com/logtriage/engine/config/src/main/java/com/logtriage/engine/config/src/main/java/com/logtriage/engine/config/src/main/java/com/logtriage/engine/config/src/main/java/com/logtriage/engine/consumer/src/main/java/com/logtriage/engine/controller/src/main/java/com/logtriage/engine/entity/src/main/java/com/logtriage/engine/exception/src/main/java/com/logtriage/engine/exception/src/main/java/com/logtriage/engine/exception/src/main/java/com/logtriage/engine/model/src/main/java/com/logtriage/engine/model/src/main/java/com/logtriage/engine/model/src/main/java/com/logtriage/engine/model/src/main/java/com/logtriage/engine/producer/src/main/java/com/logtriage/engine/producer/src/main/java/com/logtriage/engine/repository/LogEventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.logtriage.engine.repository;
-
-import com.logtriage.engine.entity.LogEventEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LogEventRepository extends JpaRepository<LogEventEntity, String> {
-}
