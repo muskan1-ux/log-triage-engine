c8e262bfdcf2eabfd001920047491d6a4ef10bc1
Muskan Sharma <muskansharma.ms66@gmail.com>
2026-09-19 14:17:11 +0530

    Create LogTriageEngineApplication class for Spring Boot

---
+++ src/main/java/com/logtriage/engine/LogTriageEngineApplication.java
@@ -0,0 +1,11 @@
+package com.logtriage.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogTriageEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LogTriageEngineApplication.class, args);
+    }
+}
